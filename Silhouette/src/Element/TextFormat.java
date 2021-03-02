15510189d4c08504784c65d1e560e7d4ef7f7a2e
norwen3 <matsengelien@gmail.com>
2021-03-02 14:49:54 +0100

    update to textformat class
    added all tags related to class in comments

--- Silhouette/src/Element/TextFormat.java
+++ Silhouette/src/Element/TextFormat.java
@@ -1,4 +1,30 @@
 package Element;
 
+//<B>
+//<bdi>
+/*
+bdo
+strong
+italics (i)
+code
+del
+em
+hr
+br
+ins
+kbd
+mark
+pre
+q
+s
+samp
+small
+sub
+sup
+u
+var
+*/
+
 public class TextFormat {
+
 }
