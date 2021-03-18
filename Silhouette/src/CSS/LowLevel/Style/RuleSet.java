01c8e93c945a2dfd74eb72eeb6263d9054af5196
Lars EriK <Lars.e.faber@gmail.com>
2021-03-18 20:13:13 +0100

    Implemented constructor and methods for RuleSet, Created new class AtRule and implemented methods for it

--- Silhouette/src/CSS/LowLevel/Style/RuleSet.java
+++ Silhouette/src/CSS/LowLevel/Style/RuleSet.java
@@ -1,46 +1,115 @@
 package CSS.LowLevel.Style;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Defines a CSS rule set containing a selector and multiple declarations.
+ */
 public class RuleSet {
-    public void setId(String idName){}
-    public void setClass(String className){}
-    public void setSelector(String value){}
-    public void setAtRule(String atRule){
-        // @keyframes, @media
-        // https://developer.mozilla.org/en-US/docs/Web/CSS/At-rule
+
+    // Instance variables
+
+    String selector;
+    ArrayList<String> rules = new ArrayList<>();
+    String atRule;
+
+    // Constructors
+
+    /**
+     * Adds "*" as default selector.
+     */
+    public RuleSet(){
+        this.selector = "*";
+    }
+    /**
+     * Initiates with one or more selectors
+     * @param selector Any type of CSS selector
+     */
+    public RuleSet(String selector){
+        // All selector types
+        setSelector(selector);
+    }
+    /**
+     * Initiates RuleSet with a selector that has an addon.
+     * @param selector Base selector
+     * @param addon Selector addon
+     */
+    public RuleSet(String selector, String addon){
+        // E[foo="bar"]
+        // E:first-child
+        // E::before
+        // E.myClass
+        setSelector(selector + addon);
+    }
+    /**
+     * Initiates RuleSet with a combination selector.
+     * @param selector1 First selector
+     * @param combinator Combinator
+     * @param selector2 Second selector
+     */
+    public RuleSet(String selector1, char combinator, String selector2){
+        // E F
+        // E > F
+        // E + F
+        // E ~ F
+        setSelector(selector1 + " " + combinator + " " + selector2);
     }
 
-    // Add rules
+    // Methods
+
+    public void setSelector(String selector){
+        this.selector = selector;
+    }
+
+    /**
+     * Adds a CSS rule as straight CSS.
+     * @param css
+     */
     public void addRule(String css){
-        // Straight up CSS
-        // e.g. Background-color: Blue
+        rules.add(css);
     }
+    /**
+     * Adds a CSS rule with property and value.
+     * @param property CSS property
+     * @param value CSS value
+     */
     public void addRule(String property, String value){
-        // Do like this:
-        // myColor.addRule("Background-color", "red");
-        //
-        // Or like this:
-        // myColor.addRule("Background-color", Color.RGB(211, 22, 3));
+        rules.add(property + " " + value);
     }
 
-    public void setDisplay(String value){}
-
-    // Box Model
-    public void setWidth(String value){}
-    public void setHeight(String value){}
-    public void setMargin(String value){}
-    public void setBorder(String value){}
-    public void setPadding(String value){}
-    public void setContent(String value){}
-
-    // Alignment
-    public void setPosition(String value){}
-    public void setFloat(String value){}
-    public void setTop(String value){}
-    public void setBottom(String value){}
-
-    // Apply to Child elements
-    public void applyToNthChild(int n, String value){
-        // .myAmazingClass:nth-child(n);
+    public void setDisplay(String value){
+        rules.add("display: " + value);
+    }
+    public void setWidth(String value){
+        rules.add("width: " + value);
+    }
+    public void setHeight(String value){
+        rules.add("height: " + value);
+    }
+    public void setMargin(String value){
+        rules.add("margin: " + value);
+    }
+    public void setBorder(String value){
+        rules.add("border: " + value);
+    }
+    public void setPadding(String value){
+        rules.add("padding: " + value);
+    }
+    public void setContent(String value){
+        rules.add("content: " + value);
+    }
+    public void setPosition(String value){
+        rules.add("position: " + value);
+    }
+    public void setFloat(String value){
+        rules.add("float: " + value);
+    }
+    public void setTop(String value){
+        rules.add("top: " + value);
+    }
+    public void setBottom(String value){
+        rules.add("bottom: " + value);
     }
 }
 
