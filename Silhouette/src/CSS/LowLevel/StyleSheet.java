640dbf756f34247894da05f13fee5b5615a4981f
Lars EriK <Lars.e.faber@gmail.com>
2021-03-19 00:36:57 +0100

    Added implementation for Stylesheet class

--- Silhouette/src/CSS/LowLevel/StyleSheet.java
+++ Silhouette/src/CSS/LowLevel/StyleSheet.java
@@ -2,7 +2,48 @@ package CSS.LowLevel;
 
 import CSS.LowLevel.Style.RuleSet;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StyleSheet {
-    void setFileName(String fileName){}
-    void applyStyle(RuleSet ruleSet){}
+
+    // Instance variables
+
+    private String fileName;
+    private ArrayList<RuleSet> ruleSets = new ArrayList<>();
+
+    // Constructor
+
+    public StyleSheet(String fileName){
+        setFileName(fileName);
+    }
+
+    public StyleSheet(String fileName, RuleSet... ruleSets){
+        setFileName(fileName);
+        this.ruleSets.addAll(Arrays.asList(ruleSets));
+    }
+
+    // Public Methods
+
+    public void setFileName(String fileName){
+        this.fileName = fileName;
+    }
+
+    public void applyStyle(RuleSet ruleSet){
+        this.ruleSets.add(ruleSet);
+    }
+
+    public void applyStyles(RuleSet... ruleSets){
+        this.ruleSets.addAll(Arrays.asList(ruleSets));
+    }
+
+    // Getters and setters
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public ArrayList<RuleSet> getRuleSets() {
+        return ruleSets;
+    }
 }
