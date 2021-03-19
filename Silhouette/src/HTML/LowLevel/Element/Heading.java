a0a0d8f68f45b8794ee766d7b1388bcd1f277e76
norwen3 <matsengelien@gmail.com>
2021-03-19 21:47:00 +0100

    Deleted Heading for merge

--- Silhouette/src/HTML/LowLevel/Element/Heading.java
+++
@@ -1,7 +0,0 @@
-package HTML.LowLevel.Element;
-
-public class Heading extends ContainerElement{
-    public Heading(String level, String text){
-        return "<h" + level + "> " + text + " <h" + level + ">";
-    }
-}
