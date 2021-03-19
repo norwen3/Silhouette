183932b94e934d38680b1d7ca00e4303e6046473
norwen3 <matsengelien@gmail.com>
2021-03-19 21:38:12 +0100

    Implemented builder in Heading
    Heading has builder implemented

--- Silhouette/src/HTML/LowLevel/Element/Heading.java
+++ Silhouette/src/HTML/LowLevel/Element/Heading.java
@@ -1,5 +1,33 @@
 package HTML.LowLevel.Element;
 
 public class Heading extends ContainerElement{
-    public Heading(String level, String text){}
+    private final int level;
+    private final String text;
+
+    public Heading(HeadingBuilder builder){
+        this.level = builder.level;
+        this.text = builder.text;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String getText() {
+        return text;
+    }
+    @Override
+    public String toString(){
+        return "<h"+level+">"+text+"</h"+level+">";
+    }
+
+    public static class HeadingBuilder {
+        private final int level;
+        private final String text;
+
+        public HeadingBuilder(int level, String text){
+            this.level = level;
+            this.text = text;
+        }
+    }
 }
