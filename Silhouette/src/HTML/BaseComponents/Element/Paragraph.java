5ccf8f2a4c0655fc8df1bbabe08930d8efe56e37
norwen3 <matsengelien@gmail.com>
2021-05-19 15:14:56 +0200

    Paragraph also javadoc
    jsj

--- Silhouette/src/HTML/BaseComponents/Element/Paragraph.java
+++ Silhouette/src/HTML/BaseComponents/Element/Paragraph.java
@@ -2,17 +2,21 @@ package HTML.BaseComponents.Element;
 
 import HTML.BaseComponents.ILowLevel.IBuilder;
 
+/**
+ * This class represents the Paragraph-tag
+ * <p></p>
+ */
 public class Paragraph extends ContainerElement {
     private String text;
 
-    // Constructors
+    /**
+     * Constructor used by builder
+     * @param builder
+     */
     private Paragraph(Builder builder){
         this.text = builder.text;
     }
 
-
-    // Methods
-    public void addText(String... text){}
 
     public String getText() {
         return text;
@@ -22,21 +26,37 @@ public class Paragraph extends ContainerElement {
     public String toString(){
         return "<p>"+text+"</p>";
     }
+
+    /**
+     * Builder class for paragraph
+     * Returns a paragraph-object
+     */
     public static class Builder implements IBuilder{
         private String text;
 
+        /**
+         * Sets the text in the p-tag to be empty
+         */
         public Builder(){
             this.text = "";
         }
 
+        /**
+         * Sets the text in the p-tag
+         * @param text
+         * @return
+         */
         public Builder setText(String text){
             this.text = text;
             return this;
         }
 
+        /**
+         * Creates a Paragraph element
+         * @return
+         */
         @Override
         public Paragraph build() {
-
             Paragraph p = new Paragraph(this);
             verify();
             return p;
